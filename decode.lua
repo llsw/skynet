3ccd303c3ee4d2905c3de7bf23b8fcceb819c96a
interfacekun <627795061@qq.com>
2016-12-16 10:10:26 +0800

    --

--- decode.lua
+++ decode.lua
@@ -1,15 +1,6 @@
 package.cpath = "luaclib/?.so"
 package.path = "lualib/?.lua;sprj/protocol/?.lua"
 
--- local crypt = require "crypt"
-
--- local password=crypt.base64decode("LOz2ElA5FzmskqtfnNxmKANgEiXoPMftWzAwdQmtH6A=")
--- print("decode password 1st", password)
--- password = crypt.aesdecode(password,"lk20-15Zp","")
-
--- print("decode password 2nd", password)
-
-
 password = crypt.aesencode("123456", "interface", "")
 print(password)
 
