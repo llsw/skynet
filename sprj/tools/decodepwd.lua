b82d7d2cd3d9c205c2da3ed60b681337fee4ce68
interfacekun <627795061@qq.com>
2016-12-16 11:47:38 +0800

    add decodepwd.lua

--- sprj/tools/decodepwd.lua
+++ sprj/tools/decodepwd.lua
@@ -9,12 +9,13 @@ print("please input who:")
 
 who = io.read()
 
-print("please input encode password:")
+print("please input decode password:")
 
 password = io.read()
 
-password = crypt.aesencode(password, who,"")
-password = crypt.base64encode(password)
+password = crypt.base64decode(password)
+password = crypt.aesdecode(password, who,"")
 
 
-print(string.format("password is encoded:%s", password))
+
+print(string.format("password is decoded:%s", password))
